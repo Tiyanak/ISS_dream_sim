3af384654764cb7774630e9df23b5045d60f2d5f
Sabzykoo <sermiskulin@gmail.com>
2017-12-07 19:12:39 +0100

    SRT

---
+++ Monetary Incentive Delay/Assets/Scripts/Client.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.DataTypes;
+using Assets.Scripts.Interfaces;
+
+namespace Assets.Scripts
+{
+    public class Client : IClient
+    {
+        public bool SendReaction(TaskType type, bool incentive, double reactionTime, double threshold)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ReceiveParameters(out double targetDisplayTime, out double cueToTargetTime, out double threshold)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
