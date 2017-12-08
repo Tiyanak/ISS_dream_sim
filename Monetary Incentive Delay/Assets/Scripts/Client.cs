e3019bcb1f98d94a424fe033ca9c14ccb8035b1a
ifarszky <ifarszky@croz.net>
2017-12-08 17:36:25 +0100

    server i client

---
+++ Monetary Incentive Delay/Assets/Scripts/Client.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour {
+
+	public Button clientBtn;
+	public Button sendBtn;
+
+	NetworkClient myClient;
+
+	void Start() {
+		Button btnClient = clientBtn.GetComponent<Button>();
+		btnClient.onClick.AddListener(SetupClient);
+
+		Button btnSend = sendBtn.GetComponent<Button>();
+		sendBtn.onClick.AddListener(SendMsg);
+	}
+
+	void SetupClient() {
+
+		myClient = new NetworkClient();
+		myClient.RegisterHandler(MsgType.Connect, OnConnected);
+		myClient.Connect("127.0.0.1", 4444);
+	}
+
+	void OnConnected(NetworkMessage netMsg) {
+		Debug.Log("Connected to server");
+	}
+
+	MessageBase GetMessage() {
+		return new StringMessage("Hello");
+	}
+
+	void SendMsg() {
+		MyMsg msg = new MyMsg();
+		msg.message = "Hello sexi";
+		myClient.Send(MsgType.Ready, msg);
+	}
+
+	
+
+}
---
+++ Monetary Incentive Delay/Assets/Scripts/Server.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Server : MonoBehaviour {
+
+	public Button serverBtn;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = serverBtn.GetComponent<Button>();
+		btn.onClick.AddListener(SetupServer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetupServer() {
+
+		NetworkServer.Listen(4444);
+		NetworkServer.RegisterHandler(MsgType.Ready, OnMsgReceived);
+
+	}
+
+	void OnMsgReceived(NetworkMessage msg) {
+		MyMsg myMsg = msg.ReadMessage<MyMsg>();
+		Debug.Log("Server received: " + myMsg.message);
+	}
+
+}
