291a9dcb24c9396dac0a582838cb3280fd04b6a0
Dean <deanbabic@hotmail.com>
2017-12-15 09:46:20 +0100

    First playable instance

---
+++ Monetary Incentive Delay/Assets/Scripts/Flashing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashing : MonoBehaviour
+{
+	public BoxType.Type BoxType;
+	public Image FlashingImage;
+	private const float EndTime = 5;
+	private float currentTime = 0;
+
+	public void Flash()
+	{
+		while (currentTime < EndTime)
+		{
+			Thread.Sleep(500);
+			FlashingImage.enabled = !FlashingImage.enabled;
+			currentTime += Time.deltaTime;
+		}
+	}
+}
