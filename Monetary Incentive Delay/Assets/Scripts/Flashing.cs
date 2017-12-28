f8e543c8066edf6b8dae11d522d8ea8863aa8fd6
ifarszky <ifarszky@croz.net>
2017-12-28 13:51:29 +0100

    dodani prefabovi za kvadratice za testiranje, umjesto png slika, te popravljena skripta za prikaz kvadratica

--- Monetary Incentive Delay/Assets/Scripts/Flashing.cs
+++ Monetary Incentive Delay/Assets/Scripts/Flashing.cs
@@ -6,17 +6,58 @@ using UnityEngine.UI;
 public class Flashing : MonoBehaviour
 {
 	public BoxType.Type BoxType;
-	public Image FlashingImage;
 	private const float EndTime = 5;
 	private float currentTime = 0;
+	private float waitTime = 0;
+	private float endWaitTime = 3;
+	public RawImage targetImage;
+	public RawImage answeredImage;
+	public bool isSpaceClicked = false;
+
+	public void Start() {
+		targetImage.enabled = false;
+		answeredImage.enabled = false;
+	}
+
+	public void Update() {
+
+		Flash();
+
+	}
 
 	public void Flash()
 	{
-		while (currentTime < EndTime)
-		{
-			Thread.Sleep(500);
-			FlashingImage.enabled = !FlashingImage.enabled;
+		// wait until its time to show target square
+		if (currentTime < EndTime) {
 			currentTime += Time.deltaTime;
+		
+		// its time to show target square
+		} else {
+			
+			// show only first time when space not clicked yet
+			if (!isSpaceClicked) {
+				targetImage.enabled = true;
+			}
+
+			// when space is clicked for the first time, hide real square and show the answered one
+			if (Input.GetKeyDown("space") && !isSpaceClicked) {
+				isSpaceClicked = true;				
+				answeredImage.enabled = true;
+				targetImage.enabled = false;
+
+			// when space is clicked wait some time of showing answered square
+			} else if (isSpaceClicked) {
+				if (waitTime < endWaitTime) {
+					waitTime += Time.deltaTime;
+
+				// when wait time have ended, start the next test iteration
+				} else {
+					answeredImage.enabled = false;
+					isSpaceClicked = false;
+					currentTime = 0;
+					waitTime = 0;
+				}
+			}
 		}
 	}
 }
