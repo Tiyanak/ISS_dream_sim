56c7ae340aaab367d39707e4b00737e85b7527a2
coldwarrior5 <deanbabic@hotmail.com>
2018-01-19 19:38:39 +0100

    Here you go

--- Monetary Incentive Delay/Assets/Scripts/Handlers/SrtHandler.cs
+++ Monetary Incentive Delay/Assets/Scripts/Handlers/SrtHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Assets.Scripts.Handlers
 {
@@ -35,8 +36,34 @@ namespace Assets.Scripts.Handlers
 
             return Z * stDev + mean;
         }
-        
-        public static double GetMean(double[] results)
+
+	    public static double GetAcceptableReationTime(List<double> results)
+	    {
+		    double mean = 0;
+		    var valid = 0;
+		    foreach (var result in results)
+		    {
+			    if (result < 0) continue;
+			    mean += result;
+			    valid++;
+		    }
+		    if (valid == 0)
+			    return double.MaxValue;
+
+		    mean /= valid;
+
+		    double sumOfSquaresOfDifferences = 0;
+		    foreach (var result in results)
+		    {
+			    if (result < 0) continue;
+			    sumOfSquaresOfDifferences += Math.Pow(result - mean, 2);
+		    }
+		    double stDev = Math.Sqrt(sumOfSquaresOfDifferences / valid);
+
+		    return Z * stDev + mean;
+	    }
+
+		public static double GetMean(double[] results)
         {
             double mean = 0;
             var valid = 0;
